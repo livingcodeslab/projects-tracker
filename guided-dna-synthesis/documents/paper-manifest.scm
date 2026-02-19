d547bc2990f2c6fa887ca653889578e9df9a5bf3
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2026-02-19 10:29:05 -0600

    Initialise the Microfluidics paper
    Set up the paper's layout and verify some basic features are working
    as expected.
    
    Write basic README for building the paper.

---
+++ guided-dna-synthesis/documents/paper-manifest.scm
@@ -0,0 +1,9 @@
+(specifications->manifest
+ (list "make"
+       "coreutils"
+       "texlive"
+       "texlive-xurl"
+       "texlive-biber"
+       "texlive-biblatex"
+       "texlive-fontspec"
+       "texlive-hyperref"))
